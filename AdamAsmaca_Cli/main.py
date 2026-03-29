6aa648fa6c1ef0564fc6724e497c892848497bf8
Hatice Çolak <hclkspace@gmail.com>
2026-03-29 23:08:32 +0300

    Clean project structure and fix merged code

--- AdamAsmaca_Cli/main.py
+++ AdamAsmaca_Cli/main.py
@@ -1,6 +1,6 @@
 import random
 
-<<<<<<< HEAD
+
 adam_asmaca_sekli = [
     "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
     "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
@@ -55,52 +55,4 @@ def adam_asmaca():
 
 if __name__ == "__main__":
     adam_asmaca()
-
-
-
-
-
-
-=======
-kelime_bankasi = ["elma", "portakal", "kivi", "armut", "çilek", "hindistancevizi"]
-
-kelime = random.choice(kelime_bankasi)
-
-tahmini_kelime = ['_'] * len(kelime)
-
-tahmin_hakki = 10
-
-tahmin_edilenler = []
-
-while tahmin_hakki > 0:
-    print("\nGüncel kelime: " + " ".join(tahmini_kelime))
-
-    tahmin = input("Bir harf tahmin et: ").lower()
-
-    if not tahmin.isalpha() or len(tahmin) != 1:
-        print("Lütfen sadece bir harf gir!")
-        continue
-
-    if tahmin in tahmin_edilenler:
-        print("Bu harfi zaten denedin!")
-        continue
-
-    tahmin_edilenler.append(tahmin)
-
-    if tahmin in kelime:
-        for i in range(len(kelime)):
-            if kelime[i] == tahmin:
-                tahmini_kelime[i] = tahmin
-        print("Şanslı tahmin!🎉")
-    else:
-        tahmin_hakki -= 1
-        print(f"Yanlış tahmin! 😅 Kalan hak: {tahmin_hakki}")
-
-    if '_' not in tahmini_kelime:
-        print(f"\nTebrikler 🎉 Kelimeyi tahmin ettin: {kelime}")
-        break
-
-if tahmin_hakki == 0 or '_' in tahmini_kelime:
-    print(f"\nTahmin hakkın bitti! Kelime: {kelime}")
->>>>>>> 825bf3be65ed5daebd3c8d18b53f3b044dfed1e4
 
